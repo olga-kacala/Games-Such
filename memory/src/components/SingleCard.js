df4979a61bd4593c8e8474f82ddba54b9a567530
olga-kacala <olga.kacala@gmail.com>
2023-03-28 21:55:33 +0200

    Beautyful pic of my dog added

--- memory/src/components/SingleCard.js
+++ memory/src/components/SingleCard.js
@@ -1,21 +1,22 @@
 import "./SingleCard.css";
 
-export function SingleCard ({card, handleChoice, flipped}) {
-const handleClick = () => {
-    handleChoice(card);
-   
+export function SingleCard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" alt="Card Front" src={card.src} />
+        <img
+          className="back"
+          alt="Card back"
+          src={"/img/cover.png"}
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
 }
-return (
-    <div className="card" >
-            <div className={flipped ? "flipped" : ""}>
-              <img className="front" alt="Card Front" src={card.src} />
-              <img
-                className="back"
-                alt="Card back"
-                src={"/img/cover.png"}
-                onClick = {handleClick}
-              />
-            </div>
-          </div>     
-)
-};
